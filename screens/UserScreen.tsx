8b454e7b0aec264fd380d8a5447ec11ea49c2532
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2022-10-29 16:09:06 +0900

    Add bottomnavigator

---
+++ screens/UserScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "native-base";
+
+const UserScreen = () => {
+    return (
+        <View>
+            <Text>
+                ユーザ画面
+            </Text>
+        </View>
+    );
+};
+
+export default UserScreen;
